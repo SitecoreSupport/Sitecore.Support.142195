b74d13ec673b21dbd331581b7ccb2d5f9a75559f
DK\mdo <mdo@sitecore.net>
2017-09-14 10:34:26 +0300

    Beacon source code and solution files

---
+++ src/Sitecore.Support.142195/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.142195")]
+[assembly: AssemblyProduct("Sitecore.Support.142195")]
+[assembly: ComVisible(false)]
